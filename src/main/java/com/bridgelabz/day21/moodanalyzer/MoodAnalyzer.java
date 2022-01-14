1e0faf0adf11b6ff74ee39c7e4337aa8351ee394
Arpit Mhaisne <95077786+arpitmh25@users.noreply.github.com>
2022-01-14 22:33:55 +0530

    Update MoodAnalyzer.java
    Updated changes

--- src/main/java/com/bridgelabz/day21/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/day21/moodanalyzer/MoodAnalyzer.java
@@ -4,9 +4,6 @@ public class MoodAnalyzer {
 
     private String msg;
 
-    public MoodAnalyzer() {
-    }
-
     public MoodAnalyzer(String msg) {
         this.msg = msg;
     }
@@ -16,7 +13,6 @@ public class MoodAnalyzer {
             if (msg.contains("Sad")) {
                 return "Sad";
             }
-            return "Happy";
         } catch (NullPointerException e) {
             return "Happy";
         }
